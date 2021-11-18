22eb5535e6f9c191121127e50a58d92db23ac945
Dominic Surrao <dsurrao@hotmail.com>
2021-11-18 09:52:48 -0500

    class 20

--- class19/todo_api_test.py
+++ class19/todo_api_test.py
@@ -9,18 +9,32 @@ https://wsvincent.com/django-rest-framework-react-tutorial/
 
 import requests
 
-base_url = <your base url here>
+base_url = <your api here>
 
 # fetch data using an HTTP GET
-resp = requests.get(base_url)
-data = resp.json()
-[print(todo['title']) for todo in data]
+# resp = requests.get(base_url)
+# data = resp.json()
+# [print(todo['title']) for todo in data]
 
-# add a todo using an HTTP POST
-my_next_todo = 'Drink kombucha'
-my_next_todo_desc = 'Drink kombucha after class'
+# add a todo using an HTTP POST (verb/method)
+my_next_todo = 'Complete class exercise today'
+my_next_todo_desc = 'Class exercise on November 18th'
 
 resp = requests.post(base_url, 
     data = {'title': my_next_todo, 'description': my_next_todo_desc})
-print(resp.text)
+data = resp.json()
+todo_id = data['id']
+print(f'newly created todo id: {todo_id}')
 
+
+
+resp = requests.put(base_url + '13/', 
+    data = {'title': my_next_todo, 'description': my_next_todo_desc})
+print(resp.json())
+
+resp = requests.patch(base_url + '13/', 
+    data = {'title': my_next_todo})
+print(resp.json())
+
+# resp = requests.delete(base_url + '12/')
+# print(resp)
